d36a6610c8c6f9034e18fbb8b54cfbf1bb02ca51
pram <pramod@pm.me>
2025-11-06 18:24:19 +0530

    fix unbound linux-lts

--- mod/services/linux.scm
+++ mod/services/linux.scm
@@ -2,6 +2,7 @@
   #:use-module (gnu packages linux)
   #:use-module (guix gexp)
   #:use-module (guix packages)
+  #:use-module (nongnu packages linux)
   #:export (linux/cxi3i716gkm))
 
 (define-public linux/cxi3i716gkm
