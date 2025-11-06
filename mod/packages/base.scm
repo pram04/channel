a25c09274d29b480999a674104881b59aacd4493
pram <pramod@pm.me>
2025-11-06 18:12:02 +0530

    intro .gitignore & guix style changes

--- mod/packages/base.scm
+++ mod/packages/base.scm
@@ -2,7 +2,6 @@
   #:use-module (gnu packages base))
 
 (define-public enIN-glibc-locales
-  (make-glibc-utf8-locales
-   glibc
-   #:locales (list "en_IN")
-   #:name "glibc-enIN-utf8-locales"))
+  (make-glibc-utf8-locales glibc
+                           #:locales (list "en_IN")
+                           #:name "glibc-enIN-utf8-locales"))
