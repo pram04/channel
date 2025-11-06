7700f1c9dc556c850ef47cc601de83a566a3fced
pram <pramod@pm.me>
2025-11-06 19:50:53 +0530

    temp fixes

--- mod/services/linux.scm
+++ mod/services/linux.scm
@@ -2,11 +2,11 @@
   #:use-module (gnu packages linux)
   #:use-module (guix gexp)
   #:use-module (guix packages)
-  #:use-module (nongnu packages linux)
+;;  #:use-module (nongnu packages linux)
   #:export (linux/cxi3i716gkm))
 
 (define-public linux/cxi3i716gkm
   (package (inherit (customize-linux
-		     #:linux linux-lts
+		     #:linux linux
 		     #:defconfig (local-file "defconfig")))
 	   (name "linux-cxi3i716gkm")))
