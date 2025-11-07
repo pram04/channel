5a37bb2720bcd4708a452204e0836fa3d800bfe5
pram <pramod@pm.me>
2025-11-07 19:02:42 +0530

    fixing acpi

--- mod/services/acpi.scm
+++ mod/services/acpi.scm
@@ -4,34 +4,55 @@
              (gnu packages base)
 	     (guix gexp))
 
-(define powerbtn-script
-  (file-union
-   "powerbtn-script"
-   `(("powerbtn.sh"
-      ,(plain-file
-        "powerbtn.sh"
-        "#!/bin/sh
-herd power-off
-")))))
+;; (define powerbtn-script
+;;   (file-union
+;;    "powerbtn-script"
+;;    `(("powerbtn.sh"
+;;       ,(plain-file
+;;         "powerbtn.sh"
+;;         "#!/bin/sh
+;; herd power-off
+;; ")))))
+
+;; (define powerbtn-event
+;;   (file-union
+;;    "powerbtn-event"
+;;    `(("events/powerbtn"
+;;       ,(plain-file
+;;         "powerbtn"
+;;         "event=button/power
+;; action=/etc/acpi/powerbtn.sh
+;; ")))))
+
 
 (define powerbtn-event
-  (file-union
-   "powerbtn-event"
-   `(("events/powerbtn"
-      ,(plain-file
-        "powerbtn"
-        "event=button/power
-action=/etc/acpi/powerbtn.sh
-")))))
+  (plain-file "powerbtn"
+              "event=button/power
+action=/etc/acpi/powerbtn.sh"))
 
-(define acpid-powerbtn-service
-  (shepherd-service
-   (auto-start? #t)
-   (documentation "ACPI power button handler")
-   (start #~(make-forkexec-constructor
-            (list (string-append #$acpid-bin "/bin/acpid") "-f")))))
+;; Define the power button handler script
+(define powerbtn-script
+  (plain-file "powerbtn.sh"
+              "#!/bin/sh
+herd power-off
+"))
 
+;; Combine files into a list for deployment
+(define acpi-files
+  (list (cons "events/powerbtn" powerbtn-event)
+        (cons "powerbtn.sh" powerbtn-script)))
+
+;; Define the Shepherd service for running acpid
 (define acpid-service
-  (simple-service 'acpid-powerbtn
-                  shepherd-root-service-type
-                  (list acpid-powerbtn-service)))
+  (service
+   (service-type
+    (name 'acpid)
+    (extensions
+     (list
+      ;; Deploy files under /etc/acpi
+      (service-extension shepherd-service-type
+        (lambda (service)
+          (list `(directory #t "/etc/acpi" ,acpi-files)
+                ;; Run acpid in foreground for Shepherd supervision
+                `(exec (string-append (assoc-ref %outputs "out") "/bin/acpid -f"))))))
+    (description "ACPI daemon with power button shutdown handling."))))
