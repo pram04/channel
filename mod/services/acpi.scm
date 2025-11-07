e0d817a0f5b57989eb7afc81964cc9435c202922
pram <pramod@pm.me>
2025-11-07 15:37:22 +0530

    fixing acpi

--- mod/services/acpi.scm
+++ mod/services/acpi.scm
@@ -5,35 +5,44 @@
 	     (guix gexp))
 
 (define powerbtn-script
-  (plain-file
-   "powerbtn.sh"
-   "#!/bin/sh
-    herd power-off
-   "))
+  (file-union
+   "powerbtn-script"
+   `(("etc/acpi/powerbtn.sh"
+      ,(plain-file
+        "powerbtn.sh"
+        "#!/bin/sh
+herd power-off
+")))))
 
+(define powerbtn-event
+  (file-union
+   "powerbtn-event"
+   `(("etc/acpi/events/powerbtn"
+      ,(plain-file
+        "powerbtn"
+        "event=button/power
+action=/etc/acpi/powerbtn.sh
+")))))
+
+(define acpi-files
+  (file-union
+   "acpi-files"
+   (list powerbtn-script powerbtn-event)))
+
+;; Define the acpid Shepherd service using file-union for files/directories
 (define acpid-service-type
   (service-type
    (name 'acpid)
    (extensions
     (list
-     (service-extension
-      shepherd-service-type
-      (lambda (service)
-        (let ((powerbtn-file (service-file service "powerbtn.sh")))
-          (list
-           `(directory #t "/etc/acpi/events")
-           `(file #t ,(service-file service "powerbtn.sh"))
-           `(file #t ,(service-file service "events/powerbtn"))
-           `(exec (string-append acpid "/bin/acpid" " -f"))
-           )))))))
-   (description "ACPI daemon for power button and ACPI event handling."))
+     (service-extension shepherd-service-type
+       (lambda (service)
+         (list
+          ;; Deploy all files from acpi-files as /etc/acpi and subdirectories
+          `(directory #t "/etc/acpi" ,acpi-files)
+          ;; Run acpid daemon in foreground with Shepherd supervision
+          `(exec (string-append (assoc-ref %outputs "out") "/bin/acpid -f")))))))
+   (description "ACPI daemon for power button handling.")))
 
 (define acpid-service
-  (service acpid-service-type
-           (files
-            (list
-             (cons "powerbtn.sh" powerbtn-script)
-             (cons "events/powerbtn"
-                   (plain-file
-                    "powerbtn"
-                    "event=button/power\naction=/etc/acpi/powerbtn.sh\n"))))))
+  (service acpid-service-type))
