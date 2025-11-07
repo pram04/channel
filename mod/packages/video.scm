9c4c9f3757da5a7cfa41678440d6550d429b26b1
pram <pramod@pm.me>
2025-11-08 00:07:39 +0530

    guix style modifications

--- mod/packages/video.scm
+++ mod/packages/video.scm
@@ -8,21 +8,47 @@
 
 (define-public mpv-js
   (package
-   (inherit mpv)
-   (version (string-append (package-version mpv) "-js"))
-   (arguments
-    (substitute-keyword-arguments (package-arguments mpv)
-				  ((#:configure-flags flags)
-				   #~(append
-				      (filter (lambda (f) (or (string=? f "-Dcdda=enabled")
-							      (string=? f "-Ddvdnav=enabled")))
-					      #$flags)
-				      ;;"-Dbuildtype=release"
-				      '("-Dalsa=enabled" "-Dcaca=disabled" "-Dcdda=disabled" "-Dcplugins=disabled" "-Ddrm=disabled" "-Ddvbin=disabled" "-Ddvdnav=disabled" "-Degl-drm=disabled" "-Degl-wayland=enabled" "-Diconv=disabled" "-Djack=disabled" "-Djavascript=enabled" "-Dlibarchive=disabled" "-Dlibbluray=disabled" "-Dmanpage-build=disabled" "-Dpipewire=enabled" "-Dpulse=disabled" "-Dsdl2=disabled" "-Dsdl2-gamepad=disabled" "-Dsixel=disabled" "-Dsndio=disabled" "-Dstrip=true" "-Dvaapi=enabled" "-Dvaapi-wayland=enabled" "-Dvulkan=enabled" "-Dwayland=enabled" "-Dx11=disabled" "-Dxv=disabled" "-Dzimg=disabled" "-Dzlib=disabled")))))
-   (native-inputs
-    (modify-inputs (package-native-inputs mpv)
-		   (delete "perl" "python-docutils")))
-   (inputs
-    (modify-inputs (package-inputs mpv)
-		   (prepend mujs)))))
+    (inherit mpv)
+    (version (string-append (package-version mpv) "-js"))
+    (arguments
+     (substitute-keyword-arguments (package-arguments mpv)
+       ((#:configure-flags flags)
+        #~(append (filter (lambda (f)
+                            (or (string=? f "-Dcdda=enabled")
+                                (string=? f "-Ddvdnav=enabled")))
+                          #$flags)
+                  ;; "-Dbuildtype=release"
+                  '("-Dalsa=enabled" "-Dcaca=disabled"
+                    "-Dcdda=disabled"
+                    "-Dcplugins=disabled"
+                    "-Ddrm=disabled"
+                    "-Ddvbin=disabled"
+                    "-Ddvdnav=disabled"
+                    "-Degl-drm=disabled"
+                    "-Degl-wayland=enabled"
+                    "-Diconv=disabled"
+                    "-Djack=disabled"
+                    "-Djavascript=enabled"
+                    "-Dlibarchive=disabled"
+                    "-Dlibbluray=disabled"
+                    "-Dmanpage-build=disabled"
+                    "-Dpipewire=enabled"
+                    "-Dpulse=disabled"
+                    "-Dsdl2=disabled"
+                    "-Dsdl2-gamepad=disabled"
+                    "-Dsixel=disabled"
+                    "-Dsndio=disabled"
+                    "-Dstrip=true"
+                    "-Dvaapi=enabled"
+                    "-Dvaapi-wayland=enabled"
+                    "-Dvulkan=enabled"
+                    "-Dwayland=enabled"
+                    "-Dx11=disabled"
+                    "-Dxv=disabled"
+                    "-Dzimg=disabled"
+                    "-Dzlib=disabled")))))
+    (native-inputs (modify-inputs (package-native-inputs mpv)
+                     (delete "perl" "python-docutils")))
+    (inputs (modify-inputs (package-inputs mpv)
+              (prepend mujs)))))
 mpv-js
--- mod/packages/wm.scm
+++ mod/packages/wm.scm
@@ -1,32 +1,35 @@
 (define-module (mod packages wm)
   #:use-module (guix build-system gnu)
-  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module ((guix licenses)
+                #:prefix license:)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages wm))
-  
+
 (define-public dwl
   (package
     (name "dwl")
     (version "0.7esc")
-    (source (local-file "/home/mod/src/dwl" #:recursive? #t))
+    (source
+     (local-file "/home/mod/src/dwl"
+                 #:recursive? #t))
     (build-system gnu-build-system)
     (arguments
-     (list #:tests? #f                  ; no tests
-           #:make-flags
-           #~(list
-              #$(string-append "CC=" (cc-for-target))
-                (string-append "PREFIX=" #$output))
-           #:phases
-           #~(modify-phases %standard-phases
-               (delete 'configure))))   ; no configure
-    (native-inputs
-     (list pkg-config wlroots))
-    (inputs
-     `(("wlroots" ,(specification->package "wlroots@0.18.2"))))
+     (list
+      #:tests? #f ;no tests
+      #:make-flags
+      #~(list #$(string-append "CC="
+                               (cc-for-target))
+              (string-append "PREFIX="
+                             #$output))
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'configure)))) ;no configure
+    (native-inputs (list pkg-config wlroots))
+    (inputs `(("wlroots" ,(specification->package "wlroots@0.18.2"))))
     (home-page "https://codeberg.org/dwl/dwl")
     (synopsis "Dynamic window manager for Wayland")
     (description
@@ -36,5 +39,5 @@ does in X11, primarily in terms of philosophy, and secondarily in terms of
 functionality.  Like dwm, dwl is easy to understand and hack on, due to a
 limited size and a few external dependencies.  It is configurable via
 @file{config.h}.")
-    ;;             LICENSE       LICENSE.dwm   LICENSE.tinywl
+    ;; LICENSE       LICENSE.dwm   LICENSE.tinywl
     (license (list license:gpl3+ license:expat license:cc0))))
