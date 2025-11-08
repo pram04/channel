2ff11c30d1d572fe914025f34312c775deb0bbc2
pram <pramod@pm.me>
2025-11-08 20:05:31 +0530

    acpi.scm fixes

--- mod/services/acpi.scm
+++ mod/services/acpi.scm
@@ -1,12 +1,31 @@
 (define-module (mod services acpi)
   #:use-module (gnu services)
   #:use-module (gnu services base)
+  #:use-module (gnu services etc)
   #:use-module (gnu services shepherd)
   #:use-module (gnu packages base)
   #:use-module (gnu packages linux)
   #:use-module (guix build utils)
   #:use-module (guix gexp)
   #:export (acpid-powerbtn-service-type))
+
+(define powerbtn-event-file
+  (plain-file
+   "powerbtn-event"
+   "event=button/power
+action=/etc/acpi/powerbtn.sh \"%e\"\n"))
+
+(define powerbtn-script-file
+  (plain-file
+   "powerbtn.sh"
+   "#!/bin/sh
+/sbin/shutdown -h now \"Power button pressed\"\n"))
+
+(define acpid-files-service
+  (simple-service 'install-acpid-powerbtn
+		  etc-service-type
+		  (list `("acpi/events/powerbtn" ,powerbtn-event-file)
+			`("acpi/powerbtn.sh" ,powerbtn-script-file))))
 
 (define acpid-shepherd-service
   (shepherd-service
@@ -19,10 +38,10 @@
    (stop #~(make-kill-destructor))
    (respawn? #t)))
 
-(define acpid-service-type
-  (service-type
-   (name 'acpid)
-   (extensions
-    (list (service-extension shepherd-root-service-type
-			     (const (list acpid-service-service)))))
-   (default-value #f)))
+;; (define acpid-service-type
+;;   (service-type
+;;    (name 'acpid)
+;;    (extensions
+;;     (list (service-extension shepherd-root-service-type
+;; 			     (const (list acpid-service-service)))))
+;;    (default-value #f)))
