391444c5d3c8bf31a997016cf437c9b8758ac4de
pram <pramod@pm.me>
2025-11-09 19:11:10 +0530

    fixing acpi

--- mod/services/acpi.scm
+++ mod/services/acpi.scm
@@ -41,10 +41,5 @@ action=/etc/acpi/powerbtn.sh \"%e\"\n"))
 			(lambda (service-config)
 			  (list
 			   `("acpi/events/powerbtn" ,powerbtn-event-file)
-			   `("acpi/powerbtn.sh" ,powerbtn-script-file))))
-     (service-extension activation-service-type
-			(lambda (service-config)
-			  #~(begin
-			      (chmod "/etc/acpi/powerbtn.sh" #o555)
-			      #t)))))
+			   `(file "acpi/powerbtn.sh" ,powerbtn-script-file #:mode #o555))))
    (default-value #f)))
