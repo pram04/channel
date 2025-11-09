d67395ad460479015a84b17a084cea73637c9c96
pram <pramod@pm.me>
2025-11-09 09:59:25 +0530

    chmod issues

--- mod/services/acpi.scm
+++ mod/services/acpi.scm
@@ -17,8 +17,8 @@ action=/etc/acpi/powerbtn.sh \"%e\"\n"))
 (define powerbtn-script-file
   (plain-file
    "powerbtn.sh"
-   "#!/bin/sh
-   /run/current-system/profile/sbin/shutdown -h now \"Power button pressed\"\n"))
+   "#!/bin/sh\n"
+   "/run/current-system/profile/sbin/shutdown -h now \"Power button pressed\"\n"))
 
 (define acpid-powerbtn-service-type
   (service-type
