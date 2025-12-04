ffeef630db084429aaedbbe84036bfbead175d78
pram <pramod@pm.me>
2025-12-04 22:47:31 +0530

    modified dwl with LOGO as MODKEY and XWayland

--- mod/packages/wm.scm
+++ mod/packages/wm.scm
@@ -3,33 +3,52 @@
   #:use-module ((guix licenses)
                 #:prefix license:)
   #:use-module (guix gexp)
+  #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages)
+  #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages wm))
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xorg))
 
-(define-public dwl
+(define-public dwl-mod
   (package
-    (name "dwl")
-    (version "0.7esc")
+    (name "dwl-mod")
+    (version "0.7")
     (source
-     (local-file "/home/mod/src/dwl"
-                 #:recursive? #t))
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+	      (url "https://codeberg.org/dwl/dwl")
+	      (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+	(base32
+	 "0404awsx8v9fyk7p2bg3p937sc56ixf8ay465xgvjcnv78hh4apd"))))
     (build-system gnu-build-system)
     (arguments
-     (list
-      #:tests? #f ;no tests
-      #:make-flags
-      #~(list #$(string-append "CC="
-                               (cc-for-target))
-              (string-append "PREFIX="
-                             #$output))
+     (list #:tests? #f ;no tests
+	   #:make-flags
+	   #~(list (string-append "CC=" #$(cc-for-target))
+		   (string-append "PREFIX=" #$output))
       #:phases
       #~(modify-phases %standard-phases
-          (delete 'configure)))) ;no configure
-    (native-inputs (list pkg-config wlroots))
-    (inputs `(("wlroots" ,(specification->package "wlroots@0.18.2"))))
+	  (delete 'configure)
+	  (add-before 'build 'changes
+	    (lambda _
+	      (let ((config-mk "config.mk")
+		    (config-def-h "config.def.h"))
+		(invoke "sed" "-i" "s/^#XWAYLAND/XWAYLAND/" config-mk)
+		(invoke "sed" "-i" "s/^#XLIBS/XLIBS/" config-mk)
+		(invoke "sed" "-i" "s/^#define MODKEY WLR_MODIFIER_ALT/#define MODKEY WLR_MODIFIER_LOGO/" config-def-h)
+		(format #t "Modified config.mk and config.def.h:~%\n")
+		(system* "tail" "-10" config-mk)
+		(system* "grep" "MODKEY" config-def-h))
+	      #t)))))
+    (native-inputs (list pkg-config wayland-protocols))
+    (inputs (list libinput libxcb wayland wlroots-0.18 libxkbcommon xcb-util-wm xorg-server-xwayland))
     (home-page "https://codeberg.org/dwl/dwl")
     (synopsis "Dynamic window manager for Wayland")
     (description
